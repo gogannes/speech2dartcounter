1ea72d3cf5cb6b0c8ee03c6b3b7ea3aff0f9bec5
gogannes <gogannes@gmail.com>
2020-10-28 18:28:21 +0100

    proper warnings in case window can't be put into focus

--- speech2dartcounter/input_dartcounter.py
+++ speech2dartcounter/input_dartcounter.py
@@ -1,5 +1,6 @@
 from pywinauto.findwindows import find_window
 from win32gui import SetForegroundWindow
+from tkinter import messagebox as mbox
 import time
 import win32com.client
 from pprint import pprint
@@ -9,7 +10,6 @@ class InputDartCounter():
     def __init__(self, window_title='DartCounter*'):
         # https://stackoverflow.com/a/2791979
         self.shell = win32com.client.Dispatch("WScript.Shell")
-
         self.window_title = window_title
 
     def checkWindow(self):
@@ -20,8 +20,15 @@ class InputDartCounter():
             return False
 
     def setForeground(self):
-        # https://stackoverflow.com/a/28548677
-        SetForegroundWindow(find_window(title_re=self.window_title))
+        success = False
+        while success == False:
+            try:
+                # https://stackoverflow.com/a/28548677
+                SetForegroundWindow(find_window(title_re=self.window_title))
+                success = True
+            except:
+                mbox.showwarning("Warning", "I could not bring window in foreground!\n"
+                                            "Try to open window 'DartCounter...' and then press 'OK'")
 
     def enter(self, sleep=0.05):
         time.sleep(sleep)
